be49f8d0e8e0e7c2049e72b851e0200411edfdba
abdulrazzaq99 <137320962+abdulrazzaq99@users.noreply.github.com>
2023-06-21 22:40:17 +0500

    Update weatherAPI.py

--- weatherAPI.py
+++ weatherAPI.py
@@ -2,13 +2,17 @@
 import requests
 
 def getWeather(location):
-     url = f"http://api.weatherapi.com/v1/current.json?key=49d241bc242143edb28183223232006&q={location}&aqi=yes"
-     response = requests.get(url)
-     data = response.json()  #As the data from web is in JSON
-     City=data['location']['name']
-     temprature=data['current']['temp_c']
-     forecast={'City':City,'temperature':temprature}
-     return forecast
+     try:
+          url = f"http://api.weatherapi.com/v1/current.json?key=49d241bc242143edb28183223232006&q={location}&aqi=yes"
+          response = requests.get(url)
+          data = response.json()  #As the data from web is in JSON
+          City=data['location']['name']
+          temprature=data['current']['temp_c']
+          forecast={'City':City,'temperature':temprature}
+          return forecast
+     except ValueError:
+          print("Location is invalid, give it another try")
+     
 
 
 
